c05376f63f202d6c07e9c846415f1625becf20dd
piotex <pkubon2@gmail.com>
2024-08-18 02:04:05 +0200

    v5

--- js/default.js
+++ js/default.js
@@ -1,6 +1,6 @@
 var listtttt = []
 var search_parameters = [{"nazwa_zam_budowlanego":"budynków"},{"data_wplywu_wniosku":"2024"}]
-var checked_names = ["nazwa_inwestor","data_wplywu_wniosku","nazwa_zam_budowlanego","kategoria", "nazwa_organu"]
+var checked_names = ["nazwa_inwestor","nazwa_zamierzenia_bud","data_wplywu_wniosku","nazwa_zam_budowlanego","kategoria", "nazwa_organu"]
 var checked_organs = ["Starosta Powiatu Warszawa Zachodnia", "Starosta Powiatu Żuromin", "Starosta Powiatu Piaseczno", "Starosta Powiatu Legionowo"]
 var checked_categories = ["I","II","III","IV","V","VI","VII","VIII"]
 
