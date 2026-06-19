22adce01b7a927064c81f01d09cbd3ac3c87b06c
Your Name <you@example.com>
2026-06-19 20:31:48 +0200

    version fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer className="app-footer">
-      <p>© {new Date().getFullYear()} CzytnikCSV | Wersja 202605</p>
+      <p>© {new Date().getFullYear()} CzytnikCSV | Wersja 202606</p>
     </footer>
   );
 };
